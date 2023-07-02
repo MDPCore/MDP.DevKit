bb6b722761713c3eb561ffacc2ebc2eb393dd855
clark159 <clark159@hotmail.com>
2023-07-02 14:27:54 +0800

    Create Project

---
+++ src/MDP.DevKit.LineMessaging/ContentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDP.DevKit.LineMessaging
+{
+    public interface ContentService
+    {
+        // Methods
+        Task<Stream> CreatePreviewContentAsync(ContentProvider contentProvider);
+
+        Task<Stream> CreateOriginalContentAsync(ContentProvider contentProvider);
+    }
+}
