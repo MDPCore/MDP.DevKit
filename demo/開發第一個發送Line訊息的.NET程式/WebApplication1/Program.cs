cd2c74f08c0866f3dd4a9d8651dd5dee0e2d3fe7
clark159 <clark159@hotmail.com>
2023-07-15 14:10:25 +0800

    Update docs

--- demo/開發第一個發送Line訊息的.NET程式/WebApplication1/Program.cs
+++ demo/開發第一個發送Line訊息的.NET程式/WebApplication1/Program.cs
@@ -8,7 +8,7 @@ namespace WebApplication1
         {
             // Builder
             var builder = WebApplication.CreateBuilder(args);
-            builder.ConfigureDefault(); // MDP.AspNetCore
+            builder.ConfigureDefault(); // 掛載MDP
 
             // App
             var app = builder.Build();
